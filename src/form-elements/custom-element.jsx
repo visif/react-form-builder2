49ebc3d8844d817bf1985bf702615afa1a186273
santobbu <santobbu@gmail.com>
2024-10-17 01:42:16 +0700

    Refactor CustomElement component to use functional components and hooks

--- src/form-elements/custom-element.jsx
+++ src/form-elements/custom-element.jsx
@@ -1,48 +1,47 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 import ComponentHeader from './component-header';
 import ComponentLabel from './component-label';
 
-class CustomElement extends Component {
-  constructor(props) {
-    super(props);
-    this.inputField = React.createRef();
+const CustomElement = (props) => {
+  const inputField = useRef(null);
+
+  const { bare } = props.data;
+  const elementProps = {
+    name: props.data.field_name,
+    defaultValue: props.defaultValue,
+  };
+
+  if (props.mutable && props.data.forwardRef) {
+    elementProps.ref = inputField;
   }
 
-  render() {
-    const { bare } = this.props.data;
-    const props = {};
-    props.name = this.props.data.field_name;
-    props.defaultValue = this.props.defaultValue;
-
-    if (this.props.mutable && this.props.data.forwardRef) {
-      props.ref = this.inputField;
-    }
-
-    if (this.props.read_only) {
-      props.disabled = 'disabled';
-    }
-
-    // Return if component is invalid.
-    if (!this.props.data.component) return null;
-    const Element = this.props.data.component;
-
-    let baseClasses = 'SortableItem rfb-item';
-    if (this.props.data.pageBreakBefore) { baseClasses += ' alwaysbreak'; }
-
-    return (
-      <div className={baseClasses} style={{ ...this.props.style }}>
-        <ComponentHeader {...this.props} />
-        { bare ?
-          <Element data={this.props.data} {...this.props.data.props} {...props} /> :
-          <div className="form-group">
-            <ComponentLabel className="form-label" {...this.props} />
-            <Element data={this.props.data} {...this.props.data.props} {...props} />
-          </div>
-        }
-      </div>
-    );
+  if (props.read_only) {
+    elementProps.disabled = 'disabled';
   }
-}
+
+  // Return if component is invalid.
+  if (!props.data.component) return null;
+  const Element = props.data.component;
+
+  let baseClasses = 'SortableItem rfb-item';
+  if (props.data.pageBreakBefore) {
+    baseClasses += ' alwaysbreak';
+  }
+
+  return (
+    <div className={baseClasses} style={{ ...props.style }}>
+      <ComponentHeader {...props} />
+      {bare ? (
+        <Element data={props.data} {...props.data.props} {...elementProps} />
+      ) : (
+        <div className="form-group">
+          <ComponentLabel className="form-label" {...props} />
+          <Element data={props.data} {...props.data.props} {...elementProps} />
+        </div>
+      )}
+    </div>
+  );
+};
 
 CustomElement.propTypes = {};
 

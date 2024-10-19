5e40401f38b932db54309815955fd3c6491d65a7
santobbu <santobbu@gmail.com>
2024-10-20 00:23:41 +0700

    Refactor Header component to use functional component and hooks

--- src/form-elements/index.jsx
+++ src/form-elements/index.jsx
@@ -14,24 +14,21 @@ import myxss from './myxss';
 
 const FormElements = {};
 
-class Header extends React.Component {
-  render() {
-    // const headerClasses = `dynamic-input ${this.props.data.element}-input`;
-    let classNames = 'static';
-    if (this.props.data.bold) { classNames += ' bold'; }
-    if (this.props.data.italic) { classNames += ' italic'; }
+const Header = (props) => {
+  let classNames = 'static';
+  if (props.data.bold) { classNames += ' bold'; }
+  if (props.data.italic) { classNames += ' italic'; }
 
-    let baseClasses = 'SortableItem rfb-item';
-    if (this.props.data.pageBreakBefore) { baseClasses += ' alwaysbreak'; }
+  let baseClasses = 'SortableItem rfb-item';
+  if (props.data.pageBreakBefore) { baseClasses += ' alwaysbreak'; }
 
-    return (
-      <div style={{ ...this.props.style }} className={baseClasses}>
-        <ComponentHeader {...this.props} />
-        <h3 className={classNames} dangerouslySetInnerHTML={{ __html: myxss.process(this.props.data.content) }} />
-      </div>
-    );
-  }
-}
+  return (
+    <div style={{ ...props.style }} className={baseClasses}>
+      <ComponentHeader {...props} />
+      <h3 className={classNames} dangerouslySetInnerHTML={{ __html: myxss.process(props.data.content) }} />
+    </div>
+  );
+};
 
 class Paragraph extends React.Component {
   render() {
